cc18858c888d63a3477b5bdd546cd62a3ae441a1
warleon <51892390+warleon@users.noreply.github.com>
2025-11-29 14:22:22 -0500

    Update ToggleIcon.cs

--- Assets/Scripts/ToggleIcon.cs
+++ Assets/Scripts/ToggleIcon.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ToggleIcon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("References")]
+    public Toggle toggle;
+    public Image iconImage;
+
+    [Header("Sprites")]
+    public Sprite soundOnSprite;
+    public Sprite soundOffSprite;
+
+    private void Awake()
     {
-        
+        // Automatically get components if not assigned
+        if (toggle == null)
+            toggle = GetComponent<Toggle>();
+        if (iconImage == null)
+            iconImage = GetComponent<Image>();
+
+        // Listen for toggle changes
+        toggle.onValueChanged.AddListener(OnToggleChanged);
+
+        // Set initial icon
+        OnToggleChanged(toggle.isOn);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        toggle.onValueChanged.RemoveListener(OnToggleChanged);
+    }
+
+    private void OnToggleChanged(bool isOn)
+    {
+        iconImage.sprite = isOn ? soundOnSprite : soundOffSprite;
     }
 }
